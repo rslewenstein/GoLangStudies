dc5f71fa79ab54d4e54d625551eab58fcb8d4860
rslewenstein <rafasoaresjf@gmail.com>
2021-02-24 23:48:41 -0300

    alterando saida do float 2 casas após a virgula

--- IMC/main.go
+++ IMC/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func VerificaImc(peso float64, altura float64) string{
@@ -9,17 +10,17 @@ func VerificaImc(peso float64, altura float64) string{
 	var imcTotal = peso / (altura * altura)
 
 	if imcTotal < 18.5{
-		msg = fmt.Sprint(imcTotal) + " Está abaixo do peso"
+		msg = fmt.Sprint(math.Floor(imcTotal * 100)/100) + " Está abaixo do peso"
 	}else if imcTotal > 18.6 && imcTotal <= 24.9 {
-		msg = fmt.Sprint(imcTotal) + " Peso ideal"
+		msg = fmt.Sprint(math.Floor(imcTotal * 100)/100) + " Peso ideal"
 	}else if imcTotal >= 25.0 && imcTotal <= 29.9 {
-		msg = fmt.Sprint(imcTotal) + " Levemente acima do peso"
+		msg = fmt.Sprint(math.Floor(imcTotal * 100)/100) + " Levemente acima do peso"
 	}else if imcTotal >= 30.0 && imcTotal <= 34.9 {
-		msg = fmt.Sprint(imcTotal) + " Obesidade grau I"
+		msg = fmt.Sprint(math.Floor(imcTotal * 100)/100) + " Obesidade grau I"
 	}else if imcTotal >= 35.0 && imcTotal <= 39.9 {
-		msg = fmt.Sprint(imcTotal) + " Obesidade grau II"
+		msg = fmt.Sprint(math.Floor(imcTotal * 100)/100) + " Obesidade grau II"
 	}else{
-		msg = fmt.Sprint(imcTotal) + " Obesidade grau III"
+		msg = fmt.Sprint(math.Floor(imcTotal * 100)/100) + " Obesidade grau III"
 	}	
 
 	return msg
